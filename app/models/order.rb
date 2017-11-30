f9de8ef363590eda67b5a73b63cb08feecc5b8b0
谢育初 <524945638@qq.com>
2017-11-30 23:39:11 +0800

    add model product_list to def CRUD order#show and add_token_to_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :product_lists
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
