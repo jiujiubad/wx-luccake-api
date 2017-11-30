3371ad73b73c43d440a241a09e99ff7e5118e593
谢育初 <524945638@qq.com>
2017-11-30 14:52:37 +0800

    CRUD products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    flash[:notice] = "已加入购物车"
+    redirect_to :back
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+  root "products#index"
+
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 
   namespace :admin do
     resources :products
