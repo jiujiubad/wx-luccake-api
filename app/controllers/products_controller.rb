28886b5113a5c5328adb4bab2bf148bb97983bd8
谢育初 <524945638@qq.com>
2017-11-30 20:50:02 +0800

    destroy_cart_item function & add_only_once_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,9 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+    end
     flash[:notice] = "已加入购物车"
     redirect_to :back
   end
