809ade228d75db935c16d4215eec1b9eb622ef53
谢育初 <524945638@qq.com>
2017-12-01 11:50:43 +0800

    gem_aasm to set 6 state

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,37 @@ class Order < ApplicationRecord
 
   before_create :generate_token
 
+  include AASM
+
+  aasm do
+    state :order_placed, :initial => true
+    state :paid
+    state :shipping
+    state :shipped
+    state :good_returned
+    state :order_canceled
+
+    event :make_payment, :after_commit => :pay! do
+      transitions :from => :order_placed, :to => :paid
+    end
+
+    event :ship do
+      transitions :from => :paid, :to => :shipping
+    end
+
+    event :deliver do
+      transitions :from => :shipping, :to => :shipped
+    end
+
+    event :return_good do
+      transitions :from => :shipped, :to => :good_returned
+    end
+
+    event :cancel_order do
+      transitions :from => [:order_placed, :paid], :to => :order_canceled
+    end
+  end
+
   def generate_token
     self.token = SecureRandom.uuid
   end
---
+++ db/migrate/20171201033147_add_aasm_state_to_order.rb
@@ -0,0 +1,5 @@
+class AddAasmStateToOrder < ActiveRecord::Migration[5.0]
+  def change
+    add_column :orders, :aasm_state, :string, default: "order_placed"
+  end
+end
