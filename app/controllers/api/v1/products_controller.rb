8a0f485d8b5d9bf644becf3903c0b6d8ac9d3830
谢育初 <524945638@qq.com>
2018-01-02 10:09:02 +0800

    edit product#search api_controller to delete paginate

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < ApiController
   before_action :validate_search_key, only: [:search]
-  
+
   def index
     @products = Product.includes(:categories, :photos).all
   end
@@ -14,7 +14,7 @@ class Api::V1::ProductsController < ApiController
   def search
     if @query_string.present?
       search_result = Product.ransack(@search_criteria).result(:distinct => true)
-      @products = search_result.paginate(:page => params[:page], :per_page => 8 )
+      @products = search_result
     end
   end
 
