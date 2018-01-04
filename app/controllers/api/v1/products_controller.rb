9703bd2b558a5626c3a0437644170fa403e76081
谢育初 <524945638@qq.com>
2018-01-04 22:41:42 +0800

    api product#search to delet paginate

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,30 +3,12 @@ class Api::V1::ProductsController < ApiController
 
   def index
     @search = Product.ransack(params[:q])
-    @products = @search.result.includes(:categories, :photos).paginate(:page => params[:page], :per_page => 8 )
+    @products = @search.result.includes(:categories, :photos)
   end
 
   def show
     @product = Product.find(params[:id])
     @photos = @product.photos.all
     @categories = @product.categories
-  end
-
-  def search
-    if @query_string.present?
-      search_result = Product.ransack(@search_criteria).result(:distinct => true)
-      @products = search_result
-    end
-  end
-
-  protected
-
-  def validate_search_key
-    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
-    @search_criteria = search_criteria(@query_string)
-  end
-
-  def search_criteria(query_string)
-    { :title_or_description_cont => query_string }
   end
 end
